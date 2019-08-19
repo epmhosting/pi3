55239d0ed03fcdb5eed7515cecd047e3bd7729e1
epmhosting <epmhosting@gmail.com>
2019-08-19 17:48:36 +0800

    update

---
+++ server/get-sensor-readings.js
@@ -0,0 +1,27 @@
+var sensor = require('node-dht-sensor')
+
+var sensorType = 22;
+var pin = 4;
+
+/*
+We abstract away the functionality to read sensor information inside the getSensorReadings function.
+This function is also asynchronous. It accepts a callback function as an argument.
+*/
+const getSensorReadings = (callback) => {
+  sensor.read(sensorType, pin, function (err, temperature, humidity) {
+    if (!err) {
+      /*
+      If there is an error, call the callback function with the error as its first argument
+      */
+      return callback(err)
+    }
+
+    /*
+    If everything went well, call the callback with "null" as the first argument to indicate thet there was no error.
+    The second and third arguments would be the results (temperature and humidty respectively)
+    */
+    callback(null, temperature, humidity)
+  })
+}
+
+module.exports = getSensorReadings
--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
+const getSensorReadings = require('./get-sensor-readings');
 
 app.get('/temperature', (req, res) => {
-  res.send('24 °C');
+  
+  getSensorReadings((err, temperature, humidity) => {
+    if (!err) {
+      res.send(temperature.toFixed(1) + '°C');
+    }
+  })
+
+  // res.send(getCachedSensorReadings.getTemperature().toFixed(1) + '°C');
+  // res.send('24 °C');
 });
 
 app.get('/humidity', (req, res) => {
-  res.send('48%');
+  
+  getSensorReadings((err, temperature, humidity) => {
+    if (!err) {
+      res.send(humidity.toFixed(1) + '%');
+    }
+  })
+
+  // res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%')
+  // res.send('48%');
 });
 
 app.get('/', (req, res) => {
